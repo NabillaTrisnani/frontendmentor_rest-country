8cc41e76d67b0a0b99703e1f229941b8a5f51a37
Nabilla Trisnani <nabillatrisnani29@gmail.com>
2022-03-16 20:54:04 +0700

    fix: theme

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import Navbar from './layout/Navbar'
 
 function App() {
   const [isActive, setActive] = useState("false");
-
+  localStorage.getItem('theme');
+  
   const handleToggle = () => {
     setActive(!isActive);
     if (isActive === true) {
-      localStorage.setItem('theme', 'light');
-    } else {
       localStorage.setItem('theme', 'dark');
+    } else {
+      localStorage.setItem('theme', 'light');
     }
   }
   return (
     <div className={localStorage.getItem('theme') === 'light' ? "App light-theme" : "App dark-theme"}>
       <Navbar
-        theme={handleToggle}
+        onClick={handleToggle}
       />
       <Routes />
     </div>
--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -8,7 +8,7 @@ class Navbar extends React.Component {
         return (
             <nav className="navbar">
                 <h1>Where in the world?</h1>
-                <button onClick={this.props.theme}><ion-icon name="moon"></ion-icon> Dark Mode</button>
+                <button onClick={this.props.onClick}><ion-icon name="moon"></ion-icon> Dark Mode</button>
             </nav>
         );
     }
